8f8ddd4170f50e1390f04d61a6720a5c6c12b72d
aditya2145 <148987683+aditya2145@users.noreply.github.com>
2024-12-07 13:34:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,23 @@
+import 'regenerator-runtime/runtime';
+// src/App.js
 import React from "react";
-import './../styles/App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ItemDetail from './ItemDetail';
-import ItemList from './ItemList';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import UserList from "./UserList";
+import UserDetail from "./UserDetail";
+
+const NotFound = () => <div>Loading...</div>;
 
 const App = () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<ItemList />} />
-                    <Route path="/users/:id" element={<ItemDetail />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="/users/:userId" element={<UserDetail />} />
+        {/* Fallback route for invalid URLs */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
